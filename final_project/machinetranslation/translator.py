ad6e7855bda0da98d8a02cd150f1d06b6779b347
opas1216@hotmail.com <opas1216@hotmail.com>
2023-04-09 14:16:06 -0400

    Temporary changes

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -1,8 +1,8 @@
-
+"""This is the practice for using API with IBM-Watson Language Translator"""
+import os
 import json
 from ibm_watson import LanguageTranslatorV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
-import os
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -24,17 +24,21 @@ language_translator.set_disable_ssl_verification(True)
 
 
 def englishToFrench(englishText):
+    """This is the method for translate english to french."""
     #write the code here
-    frenchText = ""
     translation = language_translator.translate(
     text = englishText,
-    model_id = 'en-es').get_result()
+    model_id = 'en-fr').get_result()
     print(json.dumps(translation, indent=2, ensure_ascii=False))
-    print(f"translation= {translation}")
-    # frenchText = translation[]
+    frenchText = translation['translations'][0]['translation']
     return frenchText
 
-
-# def frenchToEnglish(frenchText):
-#     #write the code here
-#     return englishText
+def frenchToEnglish(frenchText):
+    """This is the method for translate french to english."""
+    #write the code here
+    translation = language_translator.translate(
+    text = frenchText,
+    model_id = 'fr-en').get_result()
+    print(json.dumps(translation, indent=2, ensure_ascii=False))
+    englishText = translation['translations'][0]['translation']
+    return englishText
